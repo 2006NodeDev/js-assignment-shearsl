8392d1fc95e2d1a816f9ad475c28782ff146d7ba
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-14 00:57:17 -0400

    Added more comments the Fibonacci Sequence homework.

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,9 +1,12 @@
-/* 1. Fibonacci
-Define function: fib(n) 
-Return the nth number in the fibonacci sequence. */
+/* 1. Fibonacci Revature Training Homework by Laura Shears 
+Created June 14, 2020
+Last update: June 14, 2020.
+*/
+
 let n=25;
 let verb, trm;
 let fibStr = "";
+
 //Selects the correct verb and term or terms depending on whether we have a plural or singular value of n.
 if (n===1){
     verb = 'is';
@@ -14,6 +17,7 @@ else{
     trm = 'terms';
 } 
 
+//Define function: fib(n) to return the nth number in the fibonacci sequence. 
 function fib(n) {
     let term = 1;
     let priorTerm = 0;
@@ -29,7 +33,11 @@ function fib(n) {
         return term;
     }
 }
-console.log("The first " + n + " " + trm + " of the Fibonacci Sequence " + verb + ": ");
+
+//Calculate the first n terms of the sequence and save them to a string.
 for (let i=0; i<n; i++)
     fibStr += fib(i) + "  ";
+
+//Write the results to the console log.
+console.log("The first " + n + " " + trm + " of the Fibonacci Sequence " + verb + ": ");
 console.log(fibStr);
