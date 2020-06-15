244b18ed98b2a9db751640174b8deffd72ade6ce
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-15 09:48:01 -0400

    first draft of reverse string

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,14 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let newStr= "";
+    let strLength = someStr.length;
+    for (let i=strLength; i>0; i--){
+        newStr += someStr.charAt(i-1);
+    }
+    return newStr; 
 }
+
+let smStr = "DESSERTS";
+let nwStr = reverseStr(smStr);
+console.log(nwStr);
