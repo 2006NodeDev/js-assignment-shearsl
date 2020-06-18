cc27ed37172f925df89c45f3317ed32873518217
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-18 00:03:08 -0400

    first submission of even-number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,17 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    let even = false;
+    if (Math.floor(someNum/2) === Math.ceil(someNum/2))
+        even = true;
+    if (even) return someNum + " is even.";
+    else return someNum + " is not even.";
 }
+
+
+console.log(isEven(4));
+console.log(isEven(7));
+console.log(isEven(-71));
+console.log(isEven(-28));
+console.log(isEven(0));
+console.log(isEven(56.2));
