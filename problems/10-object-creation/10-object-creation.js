e19e8c78f792d2726118e354e8666c99d258f55b
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-20 16:40:57 -0400

    First submission of 10-object-creation

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,51 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+//literal
+let animal = {
+    name:'Ivy', 
+    age: 13,
+    type: 'cat'
+};
+console.log(animal);
+console.log("\n");
+
+//constructor function
+function pet(name, age, type){
+    this.name = name;
+    this.age = age;
+    this.type = type;
+}
+
+let pet1 = new pet('Ivy', 13, 'cat');
+let pet2 = new pet('Wally', 12, 'cat');
+let pet3 = new pet('Lenny', 11, 'cat');
+console.log(pet1);
+console.log(pet2);
+console.log(pet3);
+console.log("\n");
+
+//es6 class
+class cat{
+    name;
+    age;
+    likesTo;
+    constructor(name, age, likesTo){
+        this.name = name;
+        this.age = age;
+        this.likesTo = likesTo;
+    }
+    pastime(){
+        console.log(`${this.name} likes to ${this.likesTo}`);
+    }
+}
+let cat1 = new cat('Ivy', 13, 'chase and kill small animals.');
+let cat2 = new cat('Wally', 12, 'torment his sister.');
+let cat3 = new cat('Lenny', 11, 'demand that he be cuddled.');
+console.log(cat1);
+cat1.pastime();
+console.log(cat2);
+cat2.pastime();
+console.log(cat3);
+cat3.pastime();
