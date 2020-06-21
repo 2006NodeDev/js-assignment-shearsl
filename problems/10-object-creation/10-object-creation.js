dd971d8d31b2613046a9ba7175d5dc1ecd448cfe
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-20 23:56:48 -0400

    added capialization where appropriate

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -12,22 +12,22 @@ console.log(animal);
 console.log("\n");
 
 //constructor function
-function pet(name, age, type){
+function Pet(name, age, type){
     this.name = name;
     this.age = age;
     this.type = type;
 }
 
-let pet1 = new pet('Ivy', 13, 'cat');
-let pet2 = new pet('Wally', 12, 'cat');
-let pet3 = new pet('Lenny', 11, 'cat');
+let pet1 = new Pet('Ivy', 13, 'cat');
+let pet2 = new Pet('Wally', 12, 'cat');
+let pet3 = new Pet('Lenny', 11, 'cat');
 console.log(pet1);
 console.log(pet2);
 console.log(pet3);
 console.log("\n");
 
 //es6 class
-class cat{
+class Cat{
     name;
     age;
     likesTo;
@@ -40,9 +40,9 @@ class cat{
         console.log(`${this.name} likes to ${this.likesTo}`);
     }
 }
-let cat1 = new cat('Ivy', 13, 'chase and kill small animals.');
-let cat2 = new cat('Wally', 12, 'torment his sister.');
-let cat3 = new cat('Lenny', 11, 'demand that he be cuddled.');
+let cat1 = new Cat('Ivy', 13, 'chase and kill small animals.');
+let cat2 = new Cat('Wally', 12, 'torment his sister.');
+let cat3 = new Cat('Lenny', 11, 'demand that he be cuddled.');
 console.log(cat1);
 cat1.pastime();
 console.log(cat2);
